be4140122d834597f252c6bf69b1b90ac89016af
LastZoneLegends <lastzonelegends0@gmail.com>
2026-03-28 22:34:07 +0530

    Update AddMoney.jsx

--- src/pages/AddMoney.jsx
+++ src/pages/AddMoney.jsx
@@ -262,7 +262,7 @@ const handleTranzupiPayment = async () => {
   {/* Title with circle icon */}
   <div className="flex items-center gap-3 mb-4">
 
-    <div className="w-10 h-10 flex items-center justify-center rounded-full bg-blue-500/20">
+    <div className="w-10 h-10 flex items-center justify-center rounded-lg bg-blue-500/20">
       <Info className="text-blue-400" size={20} />
     </div>
 
